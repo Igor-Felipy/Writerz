bc8f2cd246c7c8938475acecd4eda189d4881051
Igor-Felipy <igor.felipy123@gmail.com>
2023-07-23 18:23:51 -0300

    Adicionando recursos

---
+++ Backend/writerz/src/main/java/com/api/writerz/WriterzApplication.java
@@ -0,0 +1,13 @@
+package com.api.writerz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WriterzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WriterzApplication.class, args);
+	}
+
+}
